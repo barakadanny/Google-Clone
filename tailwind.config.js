8707bf297d072af8f703c8cfce4c7b7eedee2d44
Danny <barakadan421@gmail.com>
2023-03-07 21:11:04 +0300

    Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  media: false,
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
