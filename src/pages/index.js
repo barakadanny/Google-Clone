cafa8903f22d243011d89ae2b02432ecf7a1853f
Danny <barakadan421@gmail.com>
2023-03-08 16:55:13 +0300

    Body implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Avatar from '../../components/Avatar'
 import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/outline"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,7 +32,29 @@ export default function Home() {
           <Avatar url="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" />
         </div>
       </header>
+
       {/* body */}
+      <form className='flex flex-col items-center mt-44 flex-grow'>
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={380}
+        />
+
+        <div 
+          className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-3 py-2 items-center sm:max-w-xl lg:max-w-2xl'
+        >
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <input type="text" className='flex-grow focus:outline-none' />
+          <MicrophoneIcon className='h-5 mr-3 text-gray-500' />
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I'm Feeling Lucky</button>
+        </div>
+      </form>
+
       {/* Footer */}
     </>
   )
