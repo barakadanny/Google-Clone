b75ec8f9619c86cff760094702c17cd70b4342f3
Danny <barakadan421@gmail.com>
2023-03-09 14:51:40 +0300

    useRouter

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,25 @@ import Avatar from '../../components/Avatar'
 import { ViewGridIcon, MicrophoneIcon } from "@heroicons/react/solid"
 import { SearchIcon } from "@heroicons/react/outline"
 import Footer from '../../components/Footer'
+import { useRef } from 'react'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = e => {
+    e.preventDefault()
+    const term = searchInputRef.current.value;
+
+    if(!term) return;
+
+    router.push(`/search?term=${term}`)
+    // window.location.href = `https://www.google.com/search?q=${e.target.value}`
+  }
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -46,13 +61,13 @@ export default function Home() {
           className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-3 py-2 items-center sm:max-w-xl lg:max-w-2xl'
         >
           <SearchIcon className='h-5 mr-3 text-gray-500' />
-          <input type="text" className='flex-grow focus:outline-none' />
+          <input ref={searchInputRef} type="text" className='flex-grow focus:outline-none' />
           <MicrophoneIcon className='h-5 mr-3 text-gray-500' />
         </div>
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button className='btn'>Google Search</button>
-          <button className='btn'>I'm Feeling Lucky</button>
+          <button onClick={search} className='btn'>Google Search</button>
+          <button onClick={search} className='btn'>I'm Feeling Lucky</button>
         </div>
       </form>
 
