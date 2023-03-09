e16d238ae17579ab37ff2d2b6d21b0c85aa7c98e
Danny <barakadan421@gmail.com>
2023-03-09 14:52:36 +0300

    Avatar updated

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Avatar({url}) {
+function Avatar({url, className}) {
   return (
     <img
         loading='lazy'
-        className='rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110'
+        className={`rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
         src={url}
         alt='avatar'
     />
