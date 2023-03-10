91ce8f78a1209c48b57554e6c9d633e580659f90
Danny <barakadan421@gmail.com>
2023-03-10 15:49:02 +0300

    Img alt

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,6 +53,7 @@ export default function Home() {
       <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt='google'
           height={100}
           width={380}
         />
