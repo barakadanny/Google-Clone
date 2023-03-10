35d1847ce595879c648dcb5d111ba4292c64b0cc
Danny <barakadan421@gmail.com>
2023-03-10 12:16:36 +0300

    Update header style

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -11,7 +11,7 @@ import {
 
 function HeaderOptions() {
   return (
-    <div className='flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]'>
+    <div className='flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b'>
         <div className='flex space-x-6'>
             <HeaderOption 
                 Icon={SearchIcon} 
