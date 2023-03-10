90f33fb3342c1f40a2c263e55f43d569640fee11
Danny <barakadan421@gmail.com>
2023-03-10 13:24:29 +0300

    Search component

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,22 +1,49 @@
 import React from 'react'
 import Head from 'next/head'
 import Header from '../../components/Header'
+import Response from '../../Response'
+import { useRouter } from 'next/router'
+import SearchResults from '../../components/SearchResults'
+// import { API_KEY, CONTEXT_KEY } from '../../Keys'
 
-function Search() {
+function Search({results}) {
+  const router = useRouter();
+
+  console.log(results)
+
   return (
     <div>
-        <Head>
-            <title>Search</title>
-            <link rel="icon" href="/favicon.ico" />
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        </Head>
-        {/* Header */}
-        <Header />
-        {/* Body */}
+      <Head>
+        <title>{router.query.term} - Google Search</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <SearchResults results={results} />
+      
+
     </div>
   )
 }
 
-export default Search
+export default Search;
+
+
+export async function getServerSideProps(context) {
+  const useDummyData = true;
+  const startIndex = context.query.start || '0'
+
+  const apiKey = process.env.API_KEY;
+  const contextKey = process.env.CONTEXT_KEY;
+
+  const data = useDummyData ? Response : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${contextKey}&q=${context.query.term}&start=${startIndex}`
+        ).then(response => response.json());
+
+  return {
+    props: {
+      results: data,
+    }
+  }
+}
